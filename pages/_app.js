4056b725179117b6ffe9748eba75cb3a45fd101f
vinhvinh <vinh.dan@r-digital.tech>
2023-01-10 10:43:59 +0700

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "../styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
